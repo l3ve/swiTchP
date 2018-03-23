13c9be0d129310c7a79a7bdd161ed6a4262b595f
libo <libo@hujiang.com>
2018-03-23 17:47:55 +0800

    0323-l3ve-解析4种滤镜算法

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ mod png;
 
 fn main() {
     let mut images = png::Img::new("./src/images/white.png");
+    let color_data = png::Img::get_color_data(&images);
+    println!("{:?}", color_data);
     let _file = png::Img::create_png("./src/images/rust.png", &mut images);
 }
--- src/png/mod.rs
+++ src/png/mod.rs
@@ -10,14 +10,14 @@ use std::collections::HashMap;
 use self::byteorder::{BigEndian, ReadBytesExt};
 use self::crc::crc32;
 use self::flate2::Compression;
-use self::flate2::write::ZlibEncoder;
+use self::flate2::write::{ZlibDecoder, ZlibEncoder};
 
 const PNG_SIGNATURE: [u8; 8] = [137, 80, 78, 71, 13, 10, 26, 10];
 const NECESSARY_CHUCKS_NAME: [&str; 4] = ["IHDR", "PLTE", "IDAT", "IEND"];
 // const IHDR: [u8; 4] = [73, 72, 68, 82];
 // const PLTE: [u8; 4] = [80, 76, 84, 69];
 // const IDAT: [u8; 4] = [73, 68, 65, 84];
-const IEND: [u8; 4] = [73, 69, 78, 68];
+// const IEND: [u8; 4] = [73, 69, 78, 68];
 
 #[derive(Debug)]
 pub struct Img<'i> {
@@ -32,7 +32,6 @@ impl<'i> Img<'i> {
     data_buffer.drain(..8);
     let chucks = Img::chuck_data(&mut data_buffer);
     let meta_data = Img::get_meta_data(chucks.get("IHDR").unwrap());
-    Img::gzip(chucks.get("IDAT").unwrap());
     Img { chucks, meta_data }
   }
   pub fn create_png(url: &str, img: &mut Img) -> Result<File> {
@@ -59,6 +58,11 @@ impl<'i> Img<'i> {
     f.read_to_end(&mut buffer)?;
     return Ok(buffer);
   }
+  pub fn get_color_data(img: &Img) -> Vec<Vec<u8>> {
+    let mut data =
+      Img::zlib_decoder(img.chucks.get("IDAT").unwrap()[0].get("chuck").unwrap()).unwrap();
+    return Img::filtering(&mut data, img);
+  }
   fn get_meta_data<'d>(ihdr: &Vec<HashMap<&str, Vec<u8>>>) -> HashMap<&'d str, u32> {
     let mut data_buffer = ihdr[0].get("chuck").unwrap().clone();
     let mut meta_data = HashMap::new();
@@ -66,14 +70,12 @@ impl<'i> Img<'i> {
     let height_length = Img::transform_to_decimal(&data_buffer.drain(..4).collect());
     let depth: &Vec<u8> = &data_buffer.drain(..1).collect();
     let color_type: Vec<u8> = data_buffer.drain(..1).collect();
-    let bpp = Img::get_bpp(3u8);
-    let x_comparison = Img::get_xcomparison(&depth[0], bpp);
+    let bpp = Img::get_bpp(color_type[0]);
     meta_data.insert("width", width_length);
     meta_data.insert("height", height_length);
     meta_data.insert("depth", depth[0] as u32);
-    meta_data.insert("colorType", color_type[0] as u32);
+    meta_data.insert("color_type", color_type[0] as u32);
     meta_data.insert("bpp", bpp);
-    meta_data.insert("xComparison", x_comparison);
     return meta_data;
   }
   fn get_bpp(ctype: u8) -> u32 {
@@ -86,10 +88,12 @@ impl<'i> Img<'i> {
     };
     return bpp;
   }
-  fn get_xcomparison(depth: &u8, bpp: u32) -> u32 {
-    let xcomparison: u32 = match *depth {
-      8u8 => bpp,
-      16u8 => (bpp * 2),
+  fn get_xcomparison(img: &Img) -> u32 {
+    let bpp = *img.meta_data.get("bpp").unwrap();
+    let depth = *img.meta_data.get("depth").unwrap();
+    let xcomparison: u32 = match depth {
+      8u32 => bpp,
+      16u32 => (bpp * 2),
       _ => bpp,
     };
     return xcomparison;
@@ -98,11 +102,123 @@ impl<'i> Img<'i> {
     let mut _crc = crc32::checksum_ieee(&content);
     return Img::transform_to_vecu8(_crc);
   }
-  fn gzip(idat: &Vec<HashMap<&str, Vec<u8>>>) {
-    // let mut e = ZlibEncoder::new(idat[0].get("chuck").unwrap(), Compression::default());
-    // e.write(b"foo");
-    // e.write(b"bar");
-    // let compressed_bytes = e.finish();
+  fn filtering(data: &mut Vec<u8>, img: &Img) -> Vec<Vec<u8>> {
+    let x_comparison = Img::get_xcomparison(img);
+    let byte_width = Img::get_byte_width(img);
+    let line_width = (byte_width + 1) as usize;
+    let loop_times = (data.len() / line_width) as usize;
+    let mut index = 0usize;
+    let mut color_data: Vec<Vec<u8>> = Vec::new();
+    while index < loop_times {
+      let mut _data: Vec<u8> = data.drain(..line_width).collect();
+      _data = match _data[0] {
+        1u8 => Img::filter_type1(_data, x_comparison),
+        2u8 => Img::filter_type2(_data, &color_data[index - 1]),
+        3u8 => Img::filter_type3(_data, x_comparison, &color_data[index - 1]),
+        4u8 => Img::filter_type4(_data, x_comparison, &color_data[index - 1]),
+        _ => _data,
+      };
+      color_data.push(_data);
+      index = index + 1;
+    }
+    return color_data;
+  }
+  fn filter_type1(data: Vec<u8>, x_comparison: u32) -> Vec<u8> {
+    let mut _data = data.clone();
+    let mut _len = _data.len() as usize;
+    let mut index = 0usize;
+    while index < _len - 1 {
+      let orig_a;
+      if index > (x_comparison - 1) as usize {
+        orig_a = _data[index - x_comparison as usize];
+      } else {
+        orig_a = 0;
+      }
+      _data[index] = _data[index + 1] + orig_a;
+      index = index + 1;
+    }
+    return _data;
+  }
+  fn filter_type2(data: Vec<u8>, upline_data: &Vec<u8>) -> Vec<u8> {
+    let mut _data = data.clone();
+    let mut _len = _data.len() as usize;
+    let mut index = 0usize;
+    while index < _len - 1 {
+      let orig_b = upline_data[index];
+      _data[index] = _data[index + 1] + orig_b;
+      index = index + 1;
+    }
+    return _data;
+  }
+  fn filter_type3(data: Vec<u8>, x_comparison: u32, upline_data: &Vec<u8>) -> Vec<u8> {
+    let mut _data = data.clone();
+    let mut _len = _data.len() as usize;
+    let mut index = 0usize;
+    while index < _len - 1 {
+      let orig_b = upline_data[index as usize];
+      let orig_a;
+      if index > (x_comparison - 1) as usize {
+        orig_a = _data[index - x_comparison as usize];
+      } else {
+        orig_a = 0;
+      }
+      _data[index] = _data[index + 1] + (orig_a + orig_b) / 2;
+      index = index + 1;
+    }
+    return _data;
+  }
+  fn filter_type4(data: Vec<u8>, x_comparison: u32, upline_data: &Vec<u8>) -> Vec<u8> {
+    let mut _data = data.clone();
+    let mut _len = _data.len() as usize;
+    let mut index = 0usize;
+    while index < _len - 1 {
+      let orig_a;
+      let orig_b = upline_data[index as usize];
+      let orig_c = upline_data[index - 1];
+      if index > (x_comparison - 1) as usize {
+        orig_a = _data[index - x_comparison as usize];
+      } else {
+        orig_a = 0;
+      }
+      let p = orig_a + orig_b - orig_c;
+      let pa = (p - orig_a) as isize;
+      let pa = pa.abs();
+      let pb = (p - orig_b) as isize;
+      let pb = pb.abs();
+      let pc = (p - orig_c) as isize;
+      let pc = pc.abs();
+      if pa <= pb && pa <= pc {
+        _data[index] = _data[index + 1] + orig_a;
+      } else if pb <= pc {
+        _data[index] = _data[index + 1] + orig_b;
+      } else {
+        _data[index] = _data[index + 1] + orig_c;
+      }
+      index = index + 1;
+    }
+    return _data;
+  }
+  fn zlib_decoder(data: &Vec<u8>) -> Result<Vec<u8>> {
+    let mut e = ZlibDecoder::new(Vec::new());
+    e.write(data)?;
+    let decoder_bytes = e.finish()?;
+    return Ok(decoder_bytes);
+  }
+  fn zlib_encoder(data: &Vec<u8>) -> Result<Vec<u8>> {
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write(&data)?;
+    let encoder_bytes = e.finish()?;
+    return Ok(encoder_bytes);
+  }
+  fn get_byte_width(img: &Img) -> u32 {
+    let width = *img.meta_data.get("width").unwrap() as f32;
+    let bpp = *img.meta_data.get("bpp").unwrap() as f32;
+    let depth = *img.meta_data.get("depth").unwrap() as f32;
+    let mut byte_width = width * bpp;
+    if depth != 8f32 {
+      byte_width = (byte_width / (8f32 / depth)).ceil();
+    }
+    return byte_width as u32;
   }
   // 转换 buffer 为十进制
   fn transform_to_decimal(buffer: &Vec<u8>) -> u32 {
